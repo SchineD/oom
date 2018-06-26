fa3edc53550b8e302d9faaf75cbfdb357a21dc6c
Daniel Schinewitz <ic17b051@technikum-wien.at>
2018-06-26 11:40:58 +0200

    add project Task7

---
+++ tasks/Task7/ConsoleApp4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using static System.Console;
+
+
+namespace Task7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.Run(() => DoSomething(1, 2000)).ContinueWith((prev) => DoSomething(4, 1000));           
+            Task.Run(() => DoSomething(2, 5000)); 
+            Task.Run(() => DoSomething(3, 1500));
+            DoMore();
+            Console.ReadLine();
+        }
+ 
+        static void DoSomething(int taskid, int sleeptime)
+        {
+            Console.WriteLine("Start of task #{0}", taskid);
+            Thread.Sleep(sleeptime);
+            Console.WriteLine("End of task #{0}", taskid);
+        }
+
+        public static async void DoMore()
+        {
+            await Task.Run(() => DoSomething(5, 3000));
+            var task6 = Task.Run(() => DoSomething(6, 1500));
+            task6.Wait();
+            Console.WriteLine("All Tasks finished");
+        }
+    }  
+}
